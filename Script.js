155666563c143e290dd29a8ca5cc5149f6963a09
Catherine <catherinecharlesfesto@gmail.com>
2025-03-29 19:58:37 +0300

    my ikl

---
+++ Script.js
@@ -0,0 +1,10 @@
+var count=0
+    function carousel(){
+    var x = document.getElementsByClassName("myslide");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+count=count=1
+    
+    }
+carousel()
